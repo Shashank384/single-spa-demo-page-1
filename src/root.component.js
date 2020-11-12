5d803d12cacac5638e5ba58e754e2daf865c44ab
Shashank384 <shashank.mayur@gmail.com>
2020-11-12 15:45:52 +0530

    page 1 - home commit

---
+++ src/root.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./root.component.css";
+
+export default function Root() {
+  return (
+    <div className="container1">
+      <h2>Welcome to Home!</h2>
+      <p>This page is built with react and implemented with single SPA framework.</p>
+    </div>
+  );
+}
